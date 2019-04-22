5b507f2ed38484b76cf998b497f91e299929ff67
supertsp <tiago.souza.pedroso@gmail.com>
2019-04-22 18:20:26 -0300

    finalizado métodos de CPU

--- src/main/java/oshi/OshiTests.java
+++ src/main/java/oshi/OshiTests.java
@@ -1,15 +1,35 @@
 package oshi;
 
+import oshi.util.Util;
+
 public class OshiTests {
 
     public static void main(String[] args) {                
         System.out.println("\n\n\n");
         
+                
+
+        
+//        String[] response = SimpleSystemInfo.getOSVersionDescription();
+//        System.out.print(" | ");
+//        for (String item : response) {
+//            System.out.print(item + " | ");
+//        }
+//        System.out.println();
         
         
-        System.out.println(SimpleSystemInfo.getCPU());
-        System.out.println(SimpleSystemInfo.getHardware());
-        System.out.println(SimpleSystemInfo.getOS());
+        for (int i = 0; i < 10_000; i++) {
+            System.out.println(
+//                    SimpleSystemInfo.getCpuCoreCurrentFrequencyAsDouble()[0] + " " +
+//                    SimpleSystemInfo.getCpuCoreCurrentFrequencyAsDouble()[1] + " " +
+//                    SimpleSystemInfo.getCpuCoreCurrentFrequencyAsDouble()[2] + " " +
+//                    SimpleSystemInfo.getCpuCoreCurrentFrequencyAsDouble()[3] + " "
+                    SimpleSystemInfo.getOsBuildVersion() + " "
+            );
+            
+            Util.sleep(100);
+        }
+        
         
         
         System.out.println("\n\n\n");
--- src/main/java/oshi/SimpleSystemInfo.java
+++ src/main/java/oshi/SimpleSystemInfo.java
@@ -1,5 +1,7 @@
 package oshi;
 
+// <editor-fold defaultstate="collapsed" desc="imports...">
+import java.util.*;
 import oshi.*;
 import oshi.data.windows.*;
 import oshi.hardware.*;
@@ -27,29 +29,37 @@ import oshi.util.platform.mac.*;
 import oshi.util.platform.unix.freebsd.*;
 import oshi.util.platform.unix.solaris.*;
 import oshi.util.platform.windows.*;
+// </editor-fold>
 
 public class SimpleSystemInfo {
 
     private static SystemInfo systemInfo = new SystemInfo();
-    
+
+    private static String singleString;
+    private static String[] arrayString;
+
+    private static long singleLong;
+    private static long[] arrayLongValues;
+    private static long[] prevTicks;
+    private static long[][] prevProcTicks;
+
+    private static double singleDouble;
+    private static double[] arrayDoubleValues;
+
     // <editor-fold defaultstate="collapsed" desc="Basic Methods... ">   
     public static SystemInfo getOshiSystemInfo() {
         return systemInfo;
     }
 
-    public static OperatingSystem getOS() {
+    public static OperatingSystem getOs() {
         return systemInfo.getOperatingSystem();
-    }
-
-    public static OperatingSystemVersion getOSVersion() {
-        return getOS().getVersion();
     }
 
     public static HardwareAbstractionLayer getHardware() {
         return systemInfo.getHardware();
     }
 
-    public static CentralProcessor getCPU() {
+    public static CentralProcessor getCpu() {
         return getHardware().getProcessor();
     }
 
@@ -58,9 +68,9 @@ public class SimpleSystemInfo {
     }
 
     public static OSProcess getProcess(int processID) {
-        return getOS().getProcess(processID);
+        return getOs().getProcess(processID);
     }
-    
+
     public static GlobalMemory getMemory() {
         return getHardware().getMemory();
     }
@@ -69,7 +79,161 @@ public class SimpleSystemInfo {
         return getHardware().getUsbDevices(showDevicesTree);
     }
     // </editor-fold>  
+
+    // <editor-fold defaultstate="collapsed" desc="OS Methods">   
+    public static String getOsName() {
+        return getOs().getFamily();
+    }
+
+    public static String getOsManufacturer() {
+        return getOs().getManufacturer();
+    }
+
+    private static String[] getOsVersionDescription() {
+        arrayString = getOs().getVersion().toString().split(" ");
+        return arrayString;
+    }
+
+    public static String getOsVersion() {
+        return getOsVersionDescription()[0];
+    }
     
+    public static String getOsBuildVersion(){
+        return getOsVersionDescription()[3];
+    }
+    // </editor-fold>  
+
+    // <editor-fold defaultstate="collapsed" desc="CPU Methods">
+    public static String getCpuManufacturer(){
+        arrayString = getCpu().getName().split(" ");
+        return arrayString[0];
+    }
     
+    public static String getCpuModel(){
+        arrayString = getCpu().getName().split(" ");
+        return arrayString[2];
+    }
     
+    public static String getCpuUsedPercentageAsString() {
+        prevTicks = getCpu().getSystemCpuLoadTicks();
+        Util.sleep(100);
+        getCpu().updateAttributes();
+        return String.format(
+                "%.2f%%",
+                (getCpu().getSystemCpuLoadBetweenTicks(prevTicks) * 100)
+        );
+    }
+
+    public static String[] getCpuCoreUsedPercentageAsString() {
+        prevProcTicks = getCpu().getProcessorCpuLoadTicks();
+        Util.sleep(100);
+        getCpu().updateAttributes();
+        double[] load = getCpu().getProcessorCpuLoadBetweenTicks(prevProcTicks);
+        arrayString = new String[load.length];
+        for (int count = 0; count < load.length; count++) {
+            arrayString[count] = String.format(" %.2f%%", (load[count] * 100));
+        }
+        return arrayString;
+    }
+
+    public static String getCpuCoreUsedPercentageAsString(int indexOfCore) {
+        return getCpuCoreUsedPercentageAsString()[indexOfCore];
+    }
+
+    public static String getCpuVendorFrequencyAsString() {
+        long freq = getCpu().getVendorFreq();
+        if (freq > 0) {
+            return FormatUtil.formatHertz(freq);
+        }
+        return "0";
+    }
+
+    public static String getCpuCurrentFrequencyAsString() {
+        Util.sleep(100);
+        getCpu().updateAttributes();
+        arrayLongValues = getCpu().getCurrentFreq();
+
+        if (arrayLongValues[0] > 0) {
+            singleLong = 0;
+            for (int count = 0; count < arrayLongValues.length; count++) {
+                singleLong += arrayLongValues[count];
+            }
+        }
+
+        singleLong /= arrayLongValues.length;
+
+        return FormatUtil.formatHertz(singleLong);
+    }
+
+    public static String[] getCpuCoreCurrentFrequencyAsString() {
+        Util.sleep(100);
+        getCpu().updateAttributes();
+        arrayLongValues = getCpu().getCurrentFreq();
+        arrayString = null;
+
+        if (arrayLongValues[0] > 0) {
+            arrayString = new String[arrayLongValues.length];
+            for (int count = 0; count < arrayLongValues.length; count++) {
+                arrayString[count] = FormatUtil.formatHertz(arrayLongValues[count]);
+            }
+        }
+
+        return arrayString;
+    }
+
+    //--- DOUBLE 
+    public static double getCpuUsedPercentageAsDouble() {
+        singleString = getCpuUsedPercentageAsString();
+        singleString = singleString.substring(0, (singleString.length() - 1));
+        singleString = singleString.replace(',', '.');
+        return Double.parseDouble(singleString);
+    }
+
+    public static double[] getCpuCoreUsedPercentageAsDouble() {
+        arrayString = getCpuCoreUsedPercentageAsString();
+        arrayDoubleValues = new double[arrayString.length];
+        for (int count = 0; count < arrayString.length; count++) {
+            singleString = arrayString[count];
+            singleString = singleString.substring(0, (singleString.length() - 1));
+            singleString = singleString.replace(',', '.');
+            arrayDoubleValues[count] = Double.parseDouble(singleString);
+        }
+        return arrayDoubleValues;
+    }
+
+    public static double getCpuCoreUsedPercentageAsDouble(int indexOfCore) {
+        return getCpuCoreUsedPercentageAsDouble()[indexOfCore];
+    }
+    
+    public static double getCpuVendorFrequencyAsDouble() {
+        singleString = getCpuCurrentFrequencyAsString();
+        singleString = singleString.substring(0, (singleString.length() - 4));
+        singleString = singleString.replace(',', '.');
+        return Double.parseDouble(singleString);
+    }
+    
+    public static double getCpuCurrentFrequencyAsDouble() {
+        singleString = getCpuCurrentFrequencyAsString();
+        singleString = singleString.substring(0, (singleString.length() - 4));
+        singleString = singleString.replace(',', '.');
+        return Double.parseDouble(singleString);
+    }
+
+    public static double[] getCpuCoreCurrentFrequencyAsDouble() {
+        arrayString = getCpuCoreCurrentFrequencyAsString();
+        arrayDoubleValues = new double[arrayString.length];
+        for (int count = 0; count < arrayString.length; count++) {
+            singleString = arrayString[count];
+            singleString = singleString.substring(0, (singleString.length() - 4));
+            singleString = singleString.replace(',', '.');
+            arrayDoubleValues[count] = Double.parseDouble(singleString);
+        }
+        return arrayDoubleValues;
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="RAM Methods">
+
+    // </editor-fold>
+
 }
